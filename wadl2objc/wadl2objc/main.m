79f4d3a5e88b9668d529013c18e0bcea39bb2fee
Dmitry Zhurov <dzhurov@provectus-it.com>
2013-08-25 11:36:53 +0300

    Initial commit

---
+++ wadl2objc/wadl2objc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  wadl2objc
+//
+//  Created by Dmitry on 8/25/13.
+//  Copyright (c) 2013 zimCo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
