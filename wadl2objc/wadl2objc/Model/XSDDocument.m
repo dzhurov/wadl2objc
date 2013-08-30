fb73a96fd2b0144fa81f45af94103a3f0e3394bb
Dima <dzhurov@provectus-it.com>
2013-08-30 17:03:44 +0300

    Enums

--- wadl2objc/wadl2objc/Model/XSDDocument.m
+++ wadl2objc/wadl2objc/Model/XSDDocument.m
@@ -23,8 +23,14 @@
 #define kSimpleTypeRestrictionKey   @"xs:restriction"
 #define kObjectSummaryKey           @"xs:element"
 
-#define kEnumFormat @"typedef NS_ENUM(NSUInteger, %@) { \n\t  %@ = 1, \n\t  %@ \n};"
+#define kEnumFormat @"typedef NS_ENUM(NSUInteger, %@) { \n\t%@ = 1, \n%@ \n};"
 #define kCheckForTextFieldFormat @"\tif ([fieldName isEqualToString:@\"%@\"]) return @\"%@\";\n"
+
+@interface XSDDocument ()
+
+@property (strong) NSString *currentFormattedDate;
+
+@end
 
 @implementation XSDDocument
 
@@ -127,6 +133,8 @@
     self.simpleTypes = [xsdSimpleTypes allValues];
 }
 
+#pragma mark - Generationg files
+
 - (void)writeObjectsToPath:(NSString *)path
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
@@ -142,13 +150,21 @@
         }
     }
     
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"yyyy.MM.dd HH:mm:ss";
+    self.currentFormattedDate = [dateFormatter stringFromDate:[NSDate date]];
+    
     for (XSDObject *object in _objects) {
-        [self writeMachineHFileObject:object toPath:path];
-        [self writeMachineMFileObject:object toPath:path];
+        [self writeMachineHFileObject:object toPath:machineFilesPath];
+        [self writeMachineMFileObject:object toPath:machineFilesPath];
+        [self writeHumanFilesForObject:object toPath:path];
+        [self writeSimpleTypes:_simpleTypes toPath:path];
     }
 }
 
 // TODO: Implement simple types parser + translators
+
+#pragma mark Machine files
 
 - (void)writeMachineHFileObject:(XSDObject*)object toPath:(NSString*)path
 {
@@ -156,7 +172,7 @@
     BOOL needForSimpleTypes = NO;
     NSString *className = [@"_" stringByAppendingString:object.name];
     NSString *fileName = [NSString stringWithFormat:@"%@.h", className];
-    NSString *filePath = [[path stringByAppendingPathComponent:kDefaultMachineFolderName] stringByAppendingPathComponent:fileName];
+    NSString *filePath = [path  stringByAppendingPathComponent:fileName];
     if ( [fileManager fileExistsAtPath:filePath] ){
         NSError *error = nil;
         [fileManager removeItemAtPath:filePath error: &error];
@@ -192,13 +208,13 @@
     }
     
     //Write To File
-    NSString *_hFilePath = [@"./Resources" stringByAppendingPathComponent:@"_ExampleEntity_h"];
+    NSString *_hFilePath = [@"./Resources" stringByAppendingPathComponent:@"_TemplateEntity_h"];
     NSError *error = nil;
     NSString *_hFileFormat = [NSString stringWithContentsOfFile:_hFilePath encoding:NSUTF8StringEncoding error: &error];
     if (error){
         NSLog(@"ERROR: %@", error);
     }
-    NSString *contentString = [NSString stringWithFormat:_hFileFormat, self.version, includes, className, propertiesList];
+    NSString *contentString = [NSString stringWithFormat:_hFileFormat, className, _currentFormattedDate, self.version, includes, className, propertiesList];
     [contentString writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
     if (error){
         NSLog(@"ERROR: %@", error);
@@ -213,7 +229,7 @@
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSString *className = [@"_" stringByAppendingString:object.name];
     NSString *fileName = [NSString stringWithFormat:@"%@.m", className];
-    NSString *filePath = [[path stringByAppendingPathComponent:kDefaultMachineFolderName] stringByAppendingPathComponent:fileName];
+    NSString *filePath = [path  stringByAppendingPathComponent:fileName];
     if ( [fileManager fileExistsAtPath:filePath] ){
         NSError *error = nil;
         [fileManager removeItemAtPath:filePath error: &error];
@@ -248,13 +264,13 @@
     }
     
     //Write To File
-    NSString *_mFilePath = [@"./Resources" stringByAppendingPathComponent:@"_ExampleEntity_m"];
+    NSString *_mFilePath = [@"./Resources" stringByAppendingPathComponent:@"_TemplateEntity_m"];
     NSError *error = nil;
     NSString *_mFileFormat = [NSString stringWithContentsOfFile:_mFilePath encoding:NSUTF8StringEncoding error: &error];
     if (error){
         NSLog(@"ERROR: %@", error);
     }
-    NSString *contentString = [NSString stringWithFormat:_mFileFormat, self.version, className, className,fieldsNamesStringKindOfObjects, fieldsNamesStringKindOfObjects, enumsConditionsList, collectionsConditionsList];
+    NSString *contentString = [NSString stringWithFormat:_mFileFormat, className, _currentFormattedDate, self.version, className, className,fieldsNamesStringKindOfObjects, fieldsNamesStringKindOfObjects, enumsConditionsList, collectionsConditionsList];
     [contentString writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
     if (error){
         NSLog(@"ERROR: %@", error);
@@ -262,8 +278,126 @@
     else{
         NSLog(@"generated: %@", filePath);
     }
-    
 }
 
+#pragma mark Human files
+
+- (void)writeHumanFilesForObject:(XSDObject*)object toPath:(NSString*)path
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *hFileName = [NSString stringWithFormat:@"%@.h", object.name];
+    NSString *hFilePath = [path  stringByAppendingPathComponent:hFileName];
+    NSString *mFileName = [NSString stringWithFormat:@"%@.m", object.name];
+    NSString *mFilePath = [path  stringByAppendingPathComponent:mFileName];
+    // .h file
+    if ( ![fileManager fileExistsAtPath:hFilePath] ){
+        NSString *hTemplateFilePath = [@"./Resources" stringByAppendingPathComponent:@"TemplateEntity_h"];
+        NSError *error = nil;
+        NSString *hFileFormat = [NSString stringWithContentsOfFile:hTemplateFilePath encoding:NSUTF8StringEncoding error: &error];
+        if (error){
+            NSLog(@"ERROR: %@", error);
+        }
+        NSString *contentString = [NSString stringWithFormat: hFileFormat, object.name, _currentFormattedDate, _version, object.name, object.name, object.name];
+        [contentString writeToFile:hFilePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
+        if (error){
+            NSLog(@"ERROR: %@", error);
+        }
+        else{
+            NSLog(@"generated: %@", hFilePath);
+        }
+    }
+    // .m file
+    if ( ![fileManager fileExistsAtPath:mFileName] ){
+        NSString *mTemplateFilePath = [@"./Resources" stringByAppendingPathComponent:@"TemplateEntity_m"];
+        NSError *error = nil;
+        NSString *mFileFormat = [NSString stringWithContentsOfFile:mTemplateFilePath encoding:NSUTF8StringEncoding error: &error];
+        if (error){
+            NSLog(@"ERROR: %@", error);
+        }
+        NSString *contentString = [NSString stringWithFormat: mFileFormat, object.name, _currentFormattedDate, _version, object.name, object.name, object.name];
+        [contentString writeToFile:mFilePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
+        if (error){
+            NSLog(@"ERROR: %@", error);
+        }
+        else{
+            NSLog(@"generated: %@", mFilePath);
+        }
+    }
+}
+
+#pragma mark Enums
+
+- (void)writeSimpleTypes:(NSArray*)simpleTypes toPath:(NSString*)path
+{
+//    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    NSMutableArray *enumsDeclaration = [NSMutableArray arrayWithCapacity:simpleTypes.count];
+    NSMutableString *enumsDictionaries = [NSMutableString string];
+    NSMutableString *enumsDictProperties = [NSMutableString string];
+    for (XSDSimpleType *oneSimpleType in simpleTypes) {
+
+        NSString *firstEnum = [oneSimpleType.name stringByAppendingString:oneSimpleType.options[0]];
+        NSMutableString *nextEnums = [NSMutableString string];
+        NSMutableString *keyValueString = [NSMutableString string];
+        NSMutableString *constStringsDeclaration = [NSMutableString string];
+        
+        for (int i = 0; i < oneSimpleType.options.count; i++) {
+            NSString *enumOptionName = [oneSimpleType.name stringByAppendingString:oneSimpleType.options[i]];
+            NSString *stringConstName = [NSString stringWithFormat:@"k%@String", enumOptionName];
+            [constStringsDeclaration appendFormat:@"static NSString *const %@ = @\"%@\";\n", stringConstName, oneSimpleType.options[i]];
+            [keyValueString appendFormat:@"@(%@) : %@", enumOptionName, stringConstName];
+            if (i < oneSimpleType.options.count - 1){
+                [keyValueString appendString:@", "];
+            }
+            if (i == 0)
+                continue;
+            [nextEnums appendFormat:@"\t%@,\n", enumOptionName];
+        }
+        [enumsDictProperties appendFormat:@"@property(nonatomic, strong) NSDictionary *%@Dictionary;\n", oneSimpleType.name];
+        [enumsDictionaries appendString:[NSString stringWithFormat:@"\t\t_%@Dictionary = @{ %@ };\n",oneSimpleType.name, keyValueString]];
+        NSString * oneEnumDecl = [NSString stringWithFormat:kEnumFormat, oneSimpleType.name, firstEnum, nextEnums];
+        [enumsDeclaration addObject:oneEnumDecl];
+        [enumsDeclaration addObject:constStringsDeclaration];
+    }
+    // .h File
+    NSString *hFileName = [NSString stringWithFormat:@"%@.h", kDefaultEnumManagerClassname];
+    NSString *hFilePath = [path  stringByAppendingPathComponent:hFileName];
+    NSString *hTemplateFilePath = [@"./Resources" stringByAppendingPathComponent:@"XSDEnums_h"];
+    NSError *error = nil;
+    NSString *hTemplateFormat = [NSString stringWithContentsOfFile:hTemplateFilePath encoding:NSUTF8StringEncoding error: &error];
+    if (error){
+        NSLog(@"ERROR: %@", error);
+        return;
+    }
+    NSString *enumsDeclarationString = [enumsDeclaration componentsJoinedByString:[NSString stringWithFormat:@"\n"]];
+    NSString *hContentString = [NSString stringWithFormat:hTemplateFormat, _currentFormattedDate, _version, enumsDeclarationString];
+    [hContentString writeToFile:hFilePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
+    if (error){
+        NSLog(@"ERROR: %@", error);
+        return;
+    }
+    else{
+        NSLog(@"generated: %@", hFilePath);
+    }
+    
+    // .m File
+    NSString *mFileName = [NSString stringWithFormat:@"%@.m", kDefaultEnumManagerClassname];
+    NSString *mFilePath = [path  stringByAppendingPathComponent:mFileName];
+    NSString *mTemplateFilePath = [@"./Resources" stringByAppendingPathComponent:@"XSDEnums_m"];
+    NSString *mTemplateFormat = [NSString stringWithContentsOfFile:mTemplateFilePath encoding:NSUTF8StringEncoding error: &error];
+    if (error){
+        NSLog(@"ERROR: %@", error);
+        return;
+    }
+    NSString *mContentString = [NSString stringWithFormat:mTemplateFormat, _currentFormattedDate, _version, enumsDictProperties, enumsDictionaries];
+    [mContentString writeToFile:mFilePath atomically:YES encoding:NSUTF8StringEncoding error: &error];
+    if (error){
+        NSLog(@"ERROR: %@", error);
+        return;
+    }
+    else{
+        NSLog(@"generated: %@", mFilePath);
+    }
+}
 
 @end
