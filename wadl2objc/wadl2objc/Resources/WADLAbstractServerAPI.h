863fafc515756345906303cfe9f5f4f92a3d718f
Dmitry Zhurov <dzhurov@provectus-it.com>
2018-01-17 16:01:44 +0200

    Removed invocation from AbstractServerAPI

--- wadl2objc/wadl2objc/Resources/WADLAbstractServerAPI.h
+++ wadl2objc/wadl2objc/Resources/WADLAbstractServerAPI.h
@@ -40,51 +40,11 @@ typedef NS_ENUM(NSInteger, WADLRequestMethod) {
 @end
 
 
-@class WADLAuthServices;
-@class WADLCustomCartServices;
-@class WADLCustomServices;
-@class WADLCustomerServices;
-@class WADLDashboardPropertiesServices;
-@class WADLFeedbackServices;
-@class WADLGroupsServices;
-@class WADLHubServices;
-@class WADLInventoryServices;
-@class WADLLogServices;
-@class WADLNotificationServices;
-@class WADLPosServices;
-@class WADLQuestionnaireServices;
-@class WADLReceiptsServices;
-@class WADLReservationServices;
-@class WADLRulesServices;
-@class WADLSearchServices;
-@class WADLSettingServices;
-@class WADLTenderServices;
-@class WADLTransactionServices;
-
+<services_classes_declaration>
 @interface WADLAbstractServerAPI : NSObject
 {
     @protected
-	WADLAuthServices *_auth;
-	WADLCustomCartServices *_customCart;
-	WADLCustomServices *_custom;
-	WADLCustomerServices *_customer;
-	WADLDashboardPropertiesServices *_dashboardProperties;
-	WADLFeedbackServices *_feedback;
-	WADLGroupsServices *_groups;
-	WADLHubServices *_hub;
-	WADLInventoryServices *_inventory;
-	WADLLogServices *_log;
-	WADLNotificationServices *_notification;
-	WADLPosServices *_pos;
-	WADLQuestionnaireServices *_questionnaire;
-	WADLReceiptsServices *_receipts;
-	WADLReservationServices *_reservation;
-	WADLRulesServices *_rules;
-	WADLSearchServices *_search;
-	WADLSettingServices *_setting;
-	WADLTenderServices *_tender;
-	WADLTransactionServices *_transaction;
-}
+<services_ivars>}
 
 @property (nonatomic, weak) id<WADLServerAPIErrorHandler> errorHanlder;
 
@@ -105,65 +65,8 @@ typedef NS_ENUM(NSInteger, WADLRequestMethod) {
 - (NSData*)serializeObject:(id)object error:(NSError **)error;
 
 #pragma Generated services
-@property (nonatomic, readonly) WADLAuthServices *auth;
-+ (WADLAuthServices*) auth;
-
-@property (nonatomic, readonly) WADLCustomCartServices *customCart;
-+ (WADLCustomCartServices*) customCart;
-
-@property (nonatomic, readonly) WADLCustomServices *custom;
-+ (WADLCustomServices*) custom;
-
-@property (nonatomic, readonly) WADLCustomerServices *customer;
-+ (WADLCustomerServices*) customer;
-
-@property (nonatomic, readonly) WADLDashboardPropertiesServices *dashboardProperties;
-+ (WADLDashboardPropertiesServices*) dashboardProperties;
-
-@property (nonatomic, readonly) WADLFeedbackServices *feedback;
-+ (WADLFeedbackServices*) feedback;
-
-@property (nonatomic, readonly) WADLGroupsServices *groups;
-+ (WADLGroupsServices*) groups;
-
-@property (nonatomic, readonly) WADLHubServices *hub;
-+ (WADLHubServices*) hub;
-
-@property (nonatomic, readonly) WADLInventoryServices *inventory;
-+ (WADLInventoryServices*) inventory;
-
-@property (nonatomic, readonly) WADLLogServices *log;
-+ (WADLLogServices*) log;
-
-@property (nonatomic, readonly) WADLNotificationServices *notification;
-+ (WADLNotificationServices*) notification;
-
-@property (nonatomic, readonly) WADLPosServices *pos;
-+ (WADLPosServices*) pos;
-
-@property (nonatomic, readonly) WADLQuestionnaireServices *questionnaire;
-+ (WADLQuestionnaireServices*) questionnaire;
-
-@property (nonatomic, readonly) WADLReceiptsServices *receipts;
-+ (WADLReceiptsServices*) receipts;
-
-@property (nonatomic, readonly) WADLReservationServices *reservation;
-+ (WADLReservationServices*) reservation;
-
-@property (nonatomic, readonly) WADLRulesServices *rules;
-+ (WADLRulesServices*) rules;
-
-@property (nonatomic, readonly) WADLSearchServices *search;
-+ (WADLSearchServices*) search;
-
-@property (nonatomic, readonly) WADLSettingServices *setting;
-+ (WADLSettingServices*) setting;
-
-@property (nonatomic, readonly) WADLTenderServices *tender;
-+ (WADLTenderServices*) tender;
-
-@property (nonatomic, readonly) WADLTransactionServices *transaction;
-+ (WADLTransactionServices*) transaction;
+<services_definition>
 
 @end
+
 
