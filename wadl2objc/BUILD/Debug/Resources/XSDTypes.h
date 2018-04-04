c163c7852431359e6d7b77bc53071ffdf4b3cb18
Dmitry Zhurov <dzhurov@provectus-it.com>
2018-04-04 17:11:13 +0300

    Made some improvements and added documentation

--- wadl2objc/BUILD/Debug/Resources/XSDTypes.h
+++ wadl2objc/BUILD/Debug/Resources/XSDTypes.h
@@ -6,6 +6,15 @@
 
 #import <Foundation/Foundation.h>
 
+
+/**
+ There are two fake classes
+ @class XSDDate
+ @class XSDDateTime
+ 
+ Don't create it directrly. Use methods `-[NSDate xsdDate]` and `-[NSDate xsdDateTime]`.
+ This classes has been create to identify xs:date and xs:dateTime in XSD Schema.
+ */
 @interface XSDDate : NSDate
 @end
 
--- wadl2objc/wadl2objc/Resources/WADLAbstractServerAPI.h
+++ wadl2objc/wadl2objc/Resources/WADLAbstractServerAPI.h
@@ -21,24 +21,23 @@ typedef NS_ENUM(NSInteger, WADLRequestMethod) {
 - (WADLRequestTask)makeRequest:(WADLRequestMethod)method
                       resource:(WADLServicesResource*)resource
                     forURLPath:(NSString *)urlPath
-               queryParameters:(NSDictionary*)queryParameters
-                    bodyObject:(NSDictionary*)parameters
-          HTTPHeaderParameters:(NSDictionary*)HTTPHeaderParameters
+               queryParameters:(NSDictionary *)queryParameters
+                    bodyObject:(NSDictionary *)parameters
+          HTTPHeaderParameters:(NSDictionary *)HTTPHeaderParameters
                    outputClass:(Class)outputClass
                  responseBlock:(void (^)(id, NSError *))responseBlock;
 
 @end
 
 
-@class MakeRequestInvocation;
+@class WADLMakeRequestInvocation;
 @protocol WADLServerAPIErrorHandler <NSObject>
-- (BOOL)serverAPI:(WADLAbstractServerAPI*)serverAPI shouldInvokeCompletionBlockWithError:(NSError*)error
-                                                                                 request:(NSURLRequest*)request
-                                                                                response:(NSURLResponse*)response
-                                                               repeatedRequestInvocation:(MakeRequestInvocation*)invocation;
+- (BOOL)serverAPI:(WADLAbstractServerAPI*)serverAPI shouldInvokeCompletionBlockWithError:(NSError *)error
+                                                                                 request:(NSURLRequest *)request
+                                                                                response:(NSURLResponse *)response
+                                                               repeatedRequestInvocation:(WADLMakeRequestInvocation *)invocation;
 
 @end
-
 
 <services_classes_declaration>
 @interface WADLAbstractServerAPI : NSObject
@@ -54,14 +53,14 @@ typedef NS_ENUM(NSInteger, WADLRequestMethod) {
  * Can be redefined in inheritor to provide different Base URLs for different schemas or targets
  * @retval "base" value in <resources> tag in .wadl file
  */
-- (NSURL *)baseURLForServicesResource:(WADLServicesResource*)resource;
+- (NSURL *)baseURLForServicesResource:(WADLServicesResource *)resource;
 
 - (NSString *)methodNameForMethod:(WADLRequestMethod)method;
 
-/*! Deserializes given data to object. Default implementation gets JSON data and deserializes it to NSDictionary or NSArray*/
+/// Deserializes given data to object. Default implementation gets JSON data and deserializes it to NSDictionary or NSArray
 - (id)deserializeData:(NSData*)data error:(NSError **)error;
 
-/*! Serializes given object to data. Default implementation gets NSDictionary or NSArray and serializes it to JSON data */
+/// Serializes given object to data. Default implementation gets NSDictionary or NSArray and serializes it to JSON data
 - (NSData*)serializeObject:(id)object error:(NSError **)error;
 
 #pragma Generated services
